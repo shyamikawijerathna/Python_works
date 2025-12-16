38d011ff8d5b6ad65bef3b3b9d3d45913503b272
Shyamika Wijerathna <shamika29.harshana@gmail.com>
2025-12-16 12:39:59 +0530

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,16 @@
+age=29
+height=1.4
+name="shyamika"
+
+is_male=True
+
+#f for formating - variables assign into string
+
+print(f"My name is:{name} | My age is:{age} | My height is:")
+
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_male))
+
+
