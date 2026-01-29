6395650ae02750c51c1eda73599205923eb8d2f8
Shyamika Wijerathna <shamika29.harshana@gmail.com>
2026-01-29 14:21:40 +0530

    Add user input functionality for name and age

---
+++ day_2/user_inputs.py
@@ -0,0 +1,12 @@
+print("Enter your name : ")
+user_name=input()
+
+print("my name is :",user_name)
+
+birth_year = int(input("enter your birth year : "))
+current_year = int(input("enter current year : "))
+
+
+age_is = current_year-birth_year
+
+print("Your age is :",age_is)
