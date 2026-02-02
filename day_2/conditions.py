3062a8ce0c06d65b78e7dd7cde590c6bb09a0d1c
Shyamika Wijerathna <shamika29.harshana@gmail.com>
2026-02-02 12:57:20 +0530

    Add name comparison functionality in conditions.py

---
+++ day_2/conditions.py
@@ -0,0 +1,8 @@
+x = input("enter your name :")
+y = input("Enter your friends name :")
+
+if x == y:
+    print("yaah my friends name matches with my name")
+    print("!!!....***")
+else:
+    print("names not match :( ")
