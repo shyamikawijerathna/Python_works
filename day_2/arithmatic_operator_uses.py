24b95ead85c0d88321eab8fa9704e1e91d379daf
Shyamika Wijerathna <shamika29.harshana@gmail.com>
2026-02-02 13:01:56 +0530

    Add arithmetic operations examples in Python
    Demonstrate various arithmetic operations including addition, multiplication, subtraction, division, floor division, power, and modulus.

---
+++ day_2/arithmatic_operator_uses.py
@@ -0,0 +1,48 @@
+
+speed_of_light=3E8
+
+print(speed_of_light)
+
+print(type(speed_of_light))
+
+
+#Arithmatic operators
+#Addition +
+print(5.3+5)
+
+#Multiply
+print(2*2)
+print(2.*2)
+print(2*2.) #If we enter a number with decimal point, result comes with decimal-Float (case sensitive)
+
+#substraction
+print(4-2)
+
+#Division
+print(2/2)
+print(2./2)
+print(2/2.)
+print(2./2.)
+
+#floor division
+print(2//2)  #result 1
+print(2.//2) #result 1.0
+
+print(5//3) #result 1
+print(5/3) #result 1.6666667
+
+#power
+print(5 ** 5) #result 3125
+print(5. ** 5) #result 3125.0
+
+#Modules-Remain value
+print(5 % 3) #result 2
+print(5. % 3) #result 2.0
+print(5. % 3.) # result 2.0
+
+print(2+6/3*3) #Result 8.0
+
+print(2 ** 2 ** 3) #calculations are starting from right - Result 256
+# print(2 / 0) division by zero error
+
+
