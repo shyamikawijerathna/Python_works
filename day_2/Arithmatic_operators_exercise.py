ceff9ea06cb4f9071edf9f01942f0d7b6ada25b7
Shyamika Wijerathna <shamika29.harshana@gmail.com>
2026-02-02 12:59:18 +0530

    Add arithmetic operations and exercises in Python
    This script performs various arithmetic operations on two integers, including addition, subtraction, multiplication, division, and power. It also includes exercises for converting kilometers to meters, manipulating variables, and swapping values.

---
+++ day_2/Arithmatic_operators_exercise.py
@@ -0,0 +1,93 @@
+#declare Two integer variables
+#perform all the arithmatic operations and print
+#performing addition 4
+
+number_1=12
+number_2=4
+
+#addition
+print("performing addition",(number_1+number_2))
+
+#substraction
+print("performing substraction",(number_1 - number_2))
+
+#multiflication
+print("performing multiflication",(number_1 * number_2))
+
+#Division
+print("performing division",(number_1 / number_2))
+
+#power
+print("performing power",(number_1 ** number_2))
+
+
+#Exercise
+print((5 * (25 % 13 + 100) / (2 * 13)) // 2)
+
+print((2 ** 4),(2 * 4.),(2 * 4))
+
+print(-2*2) # -value multiply with + value
+print(-(2*2)) # +values
+
+print(2 * 3 % 5) # answer is 1
+
+
+a=4
+b=2
+
+
+c=((a ** 2 + b ** 2) ** 0.5)
+print(round(c,2))  #built in function-  for round near decimal point 4.47
+print(round(c,1)) #4.5
+
+
+#km to meters calculator
+kilometers = 1
+
+print("km to meters = ",kilometers * 1000)
+
+kilometers = kilometers + 1 # This code short as follows
+
+kilometers += 1
+
+print(kilometers)
+
+#Excercise
+
+i=10
+j=20
+
+#increase the value of i by 2 and multiply by j
+#print the value
+
+i += 2 * j
+
+print(i)
+#Excercise
+
+x = 10
+y = 20
+z = 15
+
+var = 100
+
+#add x y z and reduct is from var variable
+
+#var= var-(x + y + z)
+
+var -= (x + y + z)
+
+print(var)
+
+#excercise
+a = 20
+b = 30
+
+#swap the values of a and b without using a third value# a
+
+
+a =(a + b )# 50
+b =(a -b)#20
+a =(a - b) #30
+
+print(a,b)
