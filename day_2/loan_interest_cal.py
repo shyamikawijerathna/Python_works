387501b4462eaea7feacdd84092742f3f2efe772
Shyamika Wijerathna <shamika29.harshana@gmail.com>
2026-02-06 16:19:05 +0530

    loan interest calculation script

---
+++ day_2/loan_interest_cal.py
@@ -0,0 +1,9 @@
+loan_amount=float(input("Loan amount :"))
+
+how_many_years =float(input("How many years :"))
+
+interest_percentage=float(input("interest percentage:"))
+
+interest=((loan_amount*interest_percentage)/100)*how_many_years
+
+print(interest)
