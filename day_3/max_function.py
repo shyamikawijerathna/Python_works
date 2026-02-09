afb24493f56c1678c6265f922246d9157f0c91af
Shyamika Wijerathna <shamika29.harshana@gmail.com>
2026-02-09 10:08:30 +0530

    Add max_function.py to compare two numbers

---
+++ day_3/max_function.py
@@ -0,0 +1,12 @@
+
+#max function
+
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number :"))
+
+larger_number = max(num1,num2)
+smaller_number = min(num1,num2)
+
+print(f"larger number is {larger_number}")
+print(f"smaller number is {smaller_number}")
