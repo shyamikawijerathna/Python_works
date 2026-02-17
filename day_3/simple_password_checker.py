0434d3b1c24f0f6177c54d67884ab698ca982f93
Shyamika Wijerathna <shamika29.harshana@gmail.com>
2026-02-17 19:54:15 +0530

    Create simple_password_checker.py

---
+++ day_3/simple_password_checker.py
@@ -0,0 +1,12 @@
+#write a simple password checker...If password match print login successful
+#else login fail
+
+enter_password = str(input("Enter password:"))
+
+if enter_password == 'shyamika':
+    print("password successful")
+
+else :
+    print("login error")
+    exit()
+print("secret data")
