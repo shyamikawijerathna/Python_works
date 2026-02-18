304e6c5abb29278d6ac30f2e751731a8af9b3a12
Shyamika Wijerathna <shamika29.harshana@gmail.com>
2026-02-18 11:57:36 +0530

    Create for_loop.py

---
+++ day_4/for_loop.py
@@ -0,0 +1,16 @@
+for i in range(10): #started from 0 value of i increase by 1 and print
+    print(i)
+    break
+
+for i in range(10,20):  # start from 10 to 19
+    print(i)
+
+
+
+for i in range(0,100):
+    print (i)
+    if i == 3:
+        print ("exiting")
+        break
+    print("hello")
+print("exited")
