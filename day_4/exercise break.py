a108aea7832ee77b07b9dc76072fd78449155092
Shyamika Wijerathna <shamika29.harshana@gmail.com>
2026-03-04 08:33:31 +0530

    Break and exit

---
+++ day_4/exercise break.py
@@ -0,0 +1,11 @@
+i = 0
+
+while i <= 100:
+    i += 1
+    print(i)
+
+    if i == 30:
+        break  #print up to 30 and program end without program exited
+    print(i)
+
+print("program exited")
